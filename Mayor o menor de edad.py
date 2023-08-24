4b644301fa6b7f4d35d16bb4ebc279c6c22bb0c9
Rodrigo Sifuentes <141968867+Rodsif@users.noreply.github.com>
2023-08-23 23:42:18 -0600

    Add files via upload
    Programas realizados en clase, usados de ejemplo para la evidencia 1

---
+++ Mayor o menor de edad.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 23 23:02:39 2023
+
+@author: Rodrigo Sifuentes.
+"""
+
+Edad = int(input("Ingrese su edad: "))
+if Edad >= 18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
---
+++ Suma de números.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 23 22:58:13 2023
+
+@author: Rodrigo Sifuentes.
+"""
+
+Numero1 = int(input("favor de ingresar el primer número: "))
+Numero2 = int(input("favor de ingresar el segundo número: "))
+
+print("La suma de los números es de: " + str(Numero1 + Numero2))
